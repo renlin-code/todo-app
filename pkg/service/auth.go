1ba64fa1b1ce9b967cba79e571876abb83516cd1
renlin-code <renelj1997@gmail.com>
2024-04-23 21:14:25 +0500

    feat: Add authentication layer

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/renlin-code/todo-app"
 	"github.com/renlin-code/todo-app/pkg/repository"
 )
 
-const salt = "hk12-sn2-5n8'd/xnwp3]v[7mqbzkv;toa"
+const (
+	salt      = "hk12-sn2-5n8'd/xnwp3]v[7mqbzkv;toa"
+	signInKey = "b|siw_wj.374*K#NSk@oda)@*2'."
+	tokenTTL  = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:user_id`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -22,6 +33,24 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signInKey))
 }
 
 func generatePasswordHash(password string) string {
